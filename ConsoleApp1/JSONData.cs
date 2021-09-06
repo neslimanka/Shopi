d00c93bab086a67f4279f9d458ac74a3d0b86672
neslimanka <43746150+neslimanka@users.noreply.github.com>
2021-09-06 23:15:25 +0300

    Add project files.

---
+++ ConsoleApp1/JSONData.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class JSONData
+    {
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+        public class Picture
+        {
+            public string url { get; set; }
+        }
+
+        public class Feature
+        {
+            public int cloudId { get; set; }
+            public string displayName { get; set; }
+            public string value { get; set; }
+            public string productId { get; set; }
+            public string gtin { get; set; }
+            public bool isProductChanger { get; set; }
+            public bool isInStock { get; set; }
+            public int order { get; set; }
+            public Picture picture { get; set; }
+        }
+
+        public class Selected
+        {
+            public int cloudId { get; set; }
+            public string displayName { get; set; }
+            public string value { get; set; }
+            public string productId { get; set; }
+            public string gtin { get; set; }
+            public bool isProductChanger { get; set; }
+            public bool isInStock { get; set; }
+            public int order { get; set; }
+            public Picture picture { get; set; }
+        }
+
+        public class Variant
+        {
+            public int cloudId { get; set; }
+            public string groupName { get; set; }
+            public string groupId { get; set; }
+            public List<Feature> features { get; set; }
+            public int order { get; set; }
+            public Selected selected { get; set; }
+        }
+
+        public class Filter
+        {
+            public string filterItemId { get; set; }
+        }
+
+        public class Picture4
+        {
+            public string url { get; set; }
+            public int? order { get; set; }
+        }
+
+        public class Category
+        {
+            public string id { get; set; }
+            public string name { get; set; }
+            public bool isRoot { get; set; }
+            public string parentCategoryId { get; set; }
+            public string description { get; set; }
+            public int order { get; set; }
+            public bool IncludeInTopMenu { get; set; }
+        }
+
+        public class Root
+        {
+            public string id { get; set; }
+            public int cloudId { get; set; }
+            public string itemId { get; set; }
+            public string productName { get; set; }
+            public string headline { get; set; }
+            public string gtin { get; set; }
+            public bool inStock { get; set; }
+            public List<Variant> variants { get; set; }
+            public List<Filter> filters { get; set; }
+            public Picture picture { get; set; }
+            public List<Picture> pictures { get; set; }
+            public string productDetailUrl { get; set; }
+            public List<object> reviews { get; set; }
+            public int quantity { get; set; }
+            public Category category { get; set; }
+            public string sku { get; set; }
+            public string ProductUrl { get; set; }
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //get();
+            Create();
+            Console.ReadLine();
+        }
+
+        public static void get()
+        {
+            var client = new RestClient("https://dev.shopiconnect.com/api/product/");
+            var request = new RestRequest("123");
+            var response = client.Execute(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                string rawResponse = response.Content;
+                JSONData result = JsonConvert.DeserializeObject<JSONData>(rawResponse);
+            }
+
+        }
+
+        public static void Create()
+        {
+            var client = new RestClient("https://dev.shopiconnect.com/api/productImport/");
+            var request = new RestRequest("create",Method.POST);
+            
+            request.AddParameter("IntegrationId","Test");
+            request.AddParameter("BaseProductCode", "Test");
+            request.AddParameter("ColorVariantCode", "Test");
+            request.AddParameter("Sku", "Test");
+            request.AddParameter("StockAmount", "Test");
+            request.AddParameter("Ean", "Test");
+            request.AddParameter("TaxRate", "Test"); 
+            request.AddParameter("Size", "Test");
+            request.AddParameter("Title", "Test");
+            request.AddParameter("Headline", "Test");
+            request.AddParameter("Description", "Test");
+            request.AddParameter("MainCategory", "Test");
+            request.AddParameter("FirstSellingVat", "Test");
+            request.AddParameter("LastSellingVat", "Test");
+            request.AddParameter("Color", "Test");
+            request.AddParameter("Image1Link", "Test");
+            request.AddParameter("Image2Link", "Test");
+            request.AddParameter("Image3Link", "Test");
+            request.AddParameter("Image4Link", "Test");
+            request.AddParameter("Image5Link", "Test");
+            request.AddParameter("WebCategory", "Test");
+            request.AddParameter("Store", "Test");
+            request.AddParameter("IsDeleted", "Test");
+            request.AddParameter("IsUnpublished", "Test");
+            request.AddParameter("Variant3", "Test");
+            request.AddParameter("Variant4", "Test");
+            request.AddParameter("Filter3", "Test");
+            request.AddParameter("Filter4", "Test");
+            request.AddParameter("Filter5", "Test");
+            request.AddParameter("Filter6", "Test");
+            request.AddHeader("Content-Type", "application/json;charset=utf-8");
+
+            var response = client.Execute(request);
+            foreach (var item in response.Content)
+            {
+                Console.WriteLine(item);
+            }
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                //string rawResponse = response.Content;
+                //JSONData result = JsonConvert.DeserializeObject<JSONData>(rawResponse);
+            }
+        }
+    }
+}
